c3fc0bd309eef6315b671dc7246cbbbcb02b2cad
Serhii Rieznik <rezniksa@gmail.com>
2024-05-12 22:38:52 +0200

    Code cleanup

--- sources/etx/render/shared/bsdf.hxx
+++ sources/etx/render/shared/bsdf.hxx
@@ -354,13 +354,13 @@ ETX_GPU_CODE SpectralResponse calculate(SpectralQuery spect, float cos_theta, co
       values.x = fresnel_generic(cos_theta, ext_ior.as_complex_x(), int_ior.as_complex_x());
       values.y = fresnel_generic(cos_theta, ext_ior.as_complex_y(), int_ior.as_complex_y());
       values.z = fresnel_generic(cos_theta, ext_ior.as_complex_z(), int_ior.as_complex_z());
-      result.components.integrated = saturate(spectrum::xyz_to_rgb(values));
+      values = spectrum::xyz_to_rgb(values);
     } else {
       values.x = fresnel_thinfilm(rgb_wl.x, cos_theta, ext_ior.as_complex_x(), thinfilm.ior.as_complex_x(), int_ior.as_complex_x(), thinfilm.thickness);
       values.y = fresnel_thinfilm(rgb_wl.y, cos_theta, ext_ior.as_complex_y(), thinfilm.ior.as_complex_y(), int_ior.as_complex_y(), thinfilm.thickness);
       values.z = fresnel_thinfilm(rgb_wl.z, cos_theta, ext_ior.as_complex_z(), thinfilm.ior.as_complex_z(), int_ior.as_complex_z(), thinfilm.thickness);
-      result.components.integrated = saturate(values);
     }
+    result.components.integrated = saturate(values);
   }
 
   return result;
